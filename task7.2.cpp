820973fb4b6a7f54c2ba30ee3b9b7dfcdb603b8d
SergeyZheleznov <jeleznov_sv@mail.ru>
2023-04-22 19:00:37 +0300

    first commit

---
+++ task7.2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <windows.h>
+#define SUB(a, b) ((a) - (b))
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    std::system("chcp 1251");
+
+    int a = 6;
+    int b = 5;
+    int c = 2;
+    
+    std::cout << SUB(a, b) << std::endl;
+    std::cout << SUB(a, b) * c << std::endl;
+    std::cout << SUB(a, b + c) * c << std::endl;
+}
+
